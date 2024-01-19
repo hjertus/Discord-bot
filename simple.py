ccfc88b3cd23712b829f14ce3e89fb87e5ad485c
hjertus <theon@hjertuslot.com>
2024-01-19 08:30:12 +0100

    trtrt

--- simple.py
+++ simple.py
@@ -1,5 +1,18 @@
 import random
 
+dadjokes = [
+    "Hvorfor kunne ikke sykkelen holde seg oppreist selv? - Fordi den var to-tung.",
+    "Hva sa den ene veggen til den andre veggen? - 'Jeg støtter deg.'",
+    "Hva kalles en fiskegruppe som spiller musikk? - En orkester.",
+    "Hvorfor tok sjøstjernen med seg et kompass til skolen? - Fordi den ville bli bedre på stjernefag.",
+    "Hvordan får du en nese til å smile? - Kjøp den en genser.",
+    "Hva gjorde den ene blyanten til den andre blyanten på skolen? - 'Du skriver meg feil.'",
+    "Hvorfor kan ikke du gi Elsa en ballong? - Fordi hun vil la den gå.",
+    "Hva sa det ene eplet til det andre eplet? - 'Slutt å dytt meg!'",
+    "Hvorfor tok ikke solen noen venner med seg til festen? - Fordi den allerede hadde nok stråler.",
+    "Hva kalles en haug med katter? - En miauskete."
+  ]
+
 kommandoer = {
     "?joke": "Jeg forteller en dårlig dad-joke.",
     '?balance': 'Sjekke hvor mye du har i banken.',
@@ -19,12 +32,9 @@ while True:
             for kommando in kommandoer:
                 print(f'{kommando} - {kommandoer[kommando]}')
         case '?balance':
-
             print(f'Du har {balance} på konto.')
-        #Hidden command
-        case ('?apekatt'):
-            money = input('Hvor mye vil du ha? ')
-            balance = balance + int(money)
+        case ('?dadjoke'):
+            print(random.choice(dadjokes))
         case ('?gamble'):
             money = input('Hvor mye vil du gamble? ')
             if random.randint(0, 2) == 1:
@@ -35,6 +45,11 @@ while True:
                 print('Du tapte')
                 balance = balance - int(money)
                 print(f'Du har nå {balance} på konto.')
+
+        # Hidden commando
+        case('?apekatt'):
+            money = input('Hvor mye vil du ha? ')
+            balance = balance + int(money)
 
 
         # stopper boten
